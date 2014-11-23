a878b6f63b3617d39303fa9e2bf6656b0d46b0db
vivraji <vivek.sundararaman@gmail.com>
2014-11-23 09:39:42 -0600

    Update cachematrix.R
    Updated Comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,7 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Matrix inversion is usually a costly computation 
+## and their may be some benefit to caching the inverse of a matrix 
+## rather than compute it repeatedly 
+## The following two functions are used to cache the inverse of a matrix
 
 # makeCacheMatrix creates a list containing a function to
 # 1. set the matrix value
